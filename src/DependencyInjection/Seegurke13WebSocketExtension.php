d6c9f74f608af6463279a013df49e486c0465056
Jerome Trope <jerometrope@gmail.com>
2020-09-07 19:44:42 +0200

    wip

--- src/DependencyInjection/Seegurke13WebSocketExtension.php
+++ src/DependencyInjection/Seegurke13WebSocketExtension.php
@@ -14,7 +14,7 @@ class Seegurke13WebSocketExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new YamlFileLoader($container, new FileLocator(dirname(__DIR__).'/Resources/config'));
+        $loader = new YamlFileLoader($container, new FileLocator(dirname(__DIR__) . '/Resources/config'));
         $loader->load('services.yml');
 
         $configuration = new Configuration($container->getParameter('kernel.debug'));
